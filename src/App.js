3f441aafe4a45b9c225a485712d96e41131a7e92
zilani131 <zilani.cuet@gmail.com>
2022-07-19 20:17:20 +0600

    New branch created

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Zilani is a good boy. He works hard</h1>
+        <h2>
+          our team members are great
+          
+        </h2>
     </div>
   );
 }
